81b9e272e03becaa249e4adf1c715b49df087073
Vayunandan Mishra <vayumishra143@gmail.com>
2026-05-30 15:23:29 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://expense-tracker-vw72.onrender.com"
 });
 
 API.interceptors.request.use((req) => {
